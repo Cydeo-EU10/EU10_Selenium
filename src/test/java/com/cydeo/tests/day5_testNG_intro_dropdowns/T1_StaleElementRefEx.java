8e38e943a34320331a6cd6183ac1b8d007420218
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-02-05 22:10:27 +0300

    day 12, 13 and review are added.

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefEx.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefEx.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.*;
 
 import java.util.concurrent.*;
 
-public class T1_StaleElementRefEx {
+public class  T1_StaleElementRefEx {
 
     public static void main(String[] args) throws InterruptedException {
 
